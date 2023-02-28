a01f0772782b42b83ce3ddd82d6a551a80fab807
JHJjun <126537023+JHJjun@users.noreply.github.com>
2023-02-28 14:17:30 +0900

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,33 @@
+#include <stdio.h.>
+#include <stdlib.h>
+
+int main()
+{
+	//int size = 10;
+	//int nums[size];
+	//return 0;
+	//printf("%d", size);
+
+	int size;
+	printf("배열길이\n");
+	scanf_s("%d", &size);
+	int* numbers = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++)
+		numbers[i] = i + 10;
+
+	for (int i = 0; i < size; i++)
+	printf("%d\n", numbers[i]);
+		
+
+	int* mynumbers = (int*)calloc(size , sizeof(int));
+	int* mynumbers2 = (int*)malloc(size * sizeof(int));
+	for (int i = 0; i < size; i++)
+	{
+		printf("calloc[%d]=%d\n", i, mynumbers[i]);
+		printf("malloc[%d]=%d\n", i, mynumbers2[i]);
+	}
+
+	free(numbers);
+
+
+}
